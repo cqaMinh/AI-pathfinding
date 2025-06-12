55e147767515a20435340873cb72f82150a1cd63
cqaMinh <146867398+cqaMinh@users.noreply.github.com>
2025-06-12 11:54:13 +0700

    Update bfs.py

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,1 +1,38 @@
 
+# algorithms/bfs.py
+from collections import deque
+import pygame
+
+def bfs_algorithms(draw, grid, start, end):
+    queue = deque([start])
+    visited = {start}
+    came_from = {}
+
+    while queue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = queue.popleft()
+
+        if current == end:
+            while current in came_from:
+                current.make_path()
+                current = came_from[current]
+            end.make_end()
+            draw()
+            return True
+
+        for neighbor in current.neighbors:
+            if neighbor not in visited and not neighbor.is_barrier():
+                visited.add(neighbor)
+                came_from[neighbor] = current
+                queue.append(neighbor)
+                neighbor.make_open()
+
+        if current != start:
+            current.make_closed()
+
+        draw()
+
+    return False
